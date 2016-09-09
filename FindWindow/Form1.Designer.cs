2a7a63ca62cc4980b100fff99671d6e3d6279d1c
smb01 <smb01@india.com>
2016-09-08 20:29:43 -0700

    Add files via upload

---
+++ FindWindow/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace FindWindow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblRect = new System.Windows.Forms.Label();
+            this.lblStyle = new System.Windows.Forms.Label();
+            this.txtHandle = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtWinText = new System.Windows.Forms.TextBox();
+            this.txtWinClass = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(270, 60);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(59, 25);
+            this.btnQuit.TabIndex = 5;
+            this.btnQuit.Text = "Cancel";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(95, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 35);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // lblRect
+            // 
+            this.lblRect.AutoSize = true;
+            this.lblRect.Location = new System.Drawing.Point(62, 219);
+            this.lblRect.Name = "lblRect";
+            this.lblRect.Size = new System.Drawing.Size(0, 13);
+            this.lblRect.TabIndex = 3;
+            // 
+            // lblStyle
+            // 
+            this.lblStyle.AutoSize = true;
+            this.lblStyle.Location = new System.Drawing.Point(62, 196);
+            this.lblStyle.Name = "lblStyle";
+            this.lblStyle.Size = new System.Drawing.Size(0, 13);
+            this.lblStyle.TabIndex = 3;
+            // 
+            // txtHandle
+            // 
+            this.txtHandle.Location = new System.Drawing.Point(62, 123);
+            this.txtHandle.Name = "txtHandle";
+            this.txtHandle.Size = new System.Drawing.Size(100, 20);
+            this.txtHandle.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 219);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Rect:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Style:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Class:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Caption:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Handle:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Finder Tool:";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(8, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 53);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Specify the window by dragging the Finder Tool over a window to select it,then re" +
+    "leasing the mouse button.";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(270, 26);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(59, 25);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtWinText);
+            this.groupBox1.Controls.Add(this.txtWinClass);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.lblRect);
+            this.groupBox1.Controls.Add(this.lblStyle);
+            this.groupBox1.Controls.Add(this.txtHandle);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(258, 251);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Criteria:";
+            // 
+            // txtWinText
+            // 
+            this.txtWinText.Location = new System.Drawing.Point(62, 146);
+            this.txtWinText.Name = "txtWinText";
+            this.txtWinText.ReadOnly = true;
+            this.txtWinText.Size = new System.Drawing.Size(179, 20);
+            this.txtWinText.TabIndex = 22;
+            // 
+            // txtWinClass
+            // 
+            this.txtWinClass.Location = new System.Drawing.Point(62, 169);
+            this.txtWinClass.Name = "txtWinClass";
+            this.txtWinClass.ReadOnly = true;
+            this.txtWinClass.Size = new System.Drawing.Size(179, 20);
+            this.txtWinClass.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 277);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FindWindow 1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FindForm_FormClosing);
+            this.Load += new System.EventHandler(this.FindForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblRect;
+        private System.Windows.Forms.Label lblStyle;
+        private System.Windows.Forms.TextBox txtHandle;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtWinText;
+        private System.Windows.Forms.TextBox txtWinClass;
+    }
+}
+
---
+++ FindWindow/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FindWindow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+		bool isMouseDowned = false;
+        int oldRop2 = 0;
+        IntPtr ghwnd = IntPtr.Zero;
+        IntPtr hdc = IntPtr.Zero;
+        IntPtr hpen = IntPtr.Zero;
+        IntPtr oldPen = IntPtr.Zero;
+
+        private void FindForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Win32.ReleaseDC(Win32.GetDesktopWindow(), hdc);
+        }
+
+        private void FindForm_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = (Properties.Resources._1).ToBitmap();
+            hdc = Win32.GetWindowDC(Win32.GetDesktopWindow());
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDowned = true;
+            pictureBox1.Image = (Properties.Resources._2).ToBitmap();
+            using (MemoryStream ms = new MemoryStream(Properties.Resources._3))
+            {
+                this.Cursor = new Cursor(ms);
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isMouseDowned)
+                return;
+
+            Win32.POINT p = new Win32.POINT();
+            Win32.RECT rect = new Win32.RECT();
+            Win32.GetCursorPos(out p);
+            IntPtr hwnd = Win32.WindowFromPoint(p);
+            if (ghwnd == hwnd)
+                return;
+            if (ghwnd != IntPtr.Zero)
+            {
+                Win32.GetWindowRect(ghwnd, out rect);
+                Win32.Rectangle(hdc, rect.Left, rect.Top, rect.Right, rect.Bottom);
+                Win32.SetROP2(hdc, oldRop2);
+                Win32.SelectObject(hdc, oldPen);
+                Win32.DeleteObject(hpen);
+            }
+
+            ghwnd = hwnd;
+            oldRop2 = Win32.SetROP2(hdc, Win32.R2_NOTXORPEN);
+            Win32.GetWindowRect(hwnd, out rect);
+            hpen = Win32.CreatePen(Win32.PS_INSIDEFRAME, 2, Win32.RGB(0, 0, 255));
+            oldPen = Win32.SelectObject(hdc, hpen);
+            Win32.Rectangle(hdc, rect.Left, rect.Top, rect.Right, rect.Bottom);
+            txtHandle.Text = "0x" + ((ulong)hwnd).ToString("X8");
+            StringBuilder sb = new StringBuilder(512);
+            Win32.GetWindowText(hwnd, sb, sb.Capacity);
+            txtWinText.Text = sb.ToString();
+            Win32.GetClassName(hwnd, sb, sb.Capacity);
+            txtWinClass.Text = sb.ToString();
+            long ulStyle = Win32.GetWindowLong(hwnd, Win32.GWL_STYLE);
+            lblStyle.Text = "0x" + ulStyle.ToString("X8");
+            lblRect.Text = "(" + rect.Left.ToString() + "," + rect.Top.ToString() + ")-(" + rect.Right.ToString() + "," + rect.Bottom.ToString() + ")," + (rect.Right - rect.Left).ToString() + "x" + (rect.Bottom - rect.Top).ToString();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDowned = false;
+            pictureBox1.Image = (Properties.Resources._1).ToBitmap();
+            this.Cursor = Cursors.Default;
+            Win32.RECT rect = new Win32.RECT();
+            Win32.GetWindowRect(ghwnd, out rect);
+            Win32.Rectangle(hdc, rect.Left, rect.Top, rect.Right, rect.Bottom);
+            Win32.SetROP2(hdc, oldRop2);
+            Win32.SelectObject(hdc, oldPen);
+            Win32.DeleteObject(hpen);
+            ghwnd = IntPtr.Zero;
+        }
+    }
+}
