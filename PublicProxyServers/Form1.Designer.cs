dd3af546222d0bddef738c7d4b3286469d92479d
smb01 <smb01@india.com>
2016-09-18 18:44:05 -0700

    Add files via upload

---
+++ PublicProxyServers/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+using System.Windows.Forms;
+
+namespace PublicProxyServers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new PublicProxyServers.ListViewEx();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 427);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(805, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exportToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(114, 54);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(110, 6);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.ShowItemToolTips = true;
+            this.listView1.Size = new System.Drawing.Size(805, 427);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Domain";
+            this.columnHeader1.Width = 128;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Rating";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 66;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Country";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 108;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Access Time";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 80;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Uptime %";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 80;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Online Since";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader6.Width = 104;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Last Test";
+            this.columnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader7.Width = 96;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Features";
+            this.columnHeader8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader8.Width = 112;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 449);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PublicProxyServers 1.0";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private ListViewEx listView1;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
+        private ColumnHeader columnHeader8;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem refreshToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripMenuItem exportToolStripMenuItem;
+    }
+}
---
+++ PublicProxyServers/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+
+namespace PublicProxyServers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        bool bBusy = false;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            refreshToolStripMenuItem_Click(sender, e);
+        }
+
+        private void GetProxyServersList(string url)
+        {
+            WebClient webclient = new WebClient();
+            string str = webclient.DownloadString(url);
+            var trs = Regex.Matches(str, @"<tr>([\s\S]*?)</tr>").OfType<Match>().Select(x => x.Groups[1].Value);
+            foreach (string tr in trs)
+            {
+                try
+                {
+                    var tds = Regex.Matches(tr, "<td.*?>(.+?)</td>").OfType<Match>().Select(x => x.Groups[1].Value).ToArray();
+                    string domain = Regex.Matches(tds[1], "<a.+?>(.+?)</a>").OfType<Match>().Select(x => x.Groups[1].Value).ToArray()[0];
+
+                    string features = "-";
+                    var select = Regex.Matches(tds[8], "<div class=\"(.+?)\">(.+?)</div>").OfType<Match>();
+                    var divs = select.Select(x => x.Groups[1].Value).ToArray();
+                    if (divs[0] == "on")
+                        features = select.Select(x => x.Groups[2].Value).ToArray()[0];
+
+                    bool ssl = false;
+                    select = Regex.Matches(tds[9], "<div class=\"(.+?)\">(.+?)</div>").OfType<Match>();
+                    divs = select.Select(x => x.Groups[1].Value).ToArray();
+                    if (divs[0] == "on")
+                    {
+                        features += " | " + select.Select(x => x.Groups[2].Value).ToArray()[0];
+                        ssl = true;
+                    }
+
+                    ListViewItem item = new ListViewItem(domain);
+                    item.SubItems.AddRange(new string[] { tds[2], tds[3], tds[4], tds[5], tds[6], tds[7], features });
+                    item.Tag = ssl;
+                    listView1.Items.Add(item);
+                }
+                catch (Exception ex) { }
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            if (bBusy)
+                refreshToolStripMenuItem.Enabled = false;
+            else
+                refreshToolStripMenuItem.Enabled = true;
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            bBusy = true;
+            toolStripStatusLabel1.Text = "Downloading servers list...";
+            Thread thread = new Thread(delegate ()
+            {
+                try
+                {
+                    for (int i = 1; i < 7; i++)
+                        GetProxyServersList("http://www.publicproxyservers.com/proxy/list" + i.ToString() + ".html");
+                    bBusy = false;
+                    toolStripStatusLabel1.Text = "Total " + listView1.Items.Count.ToString() + " servers";
+                }
+                catch(Exception ex)
+                {
+                    toolStripStatusLabel1.Text = ex.Message;
+                }
+            });
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.Export();
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            if (e.Column == listView1.CurrentColumn)
+            {
+                if (listView1.asc)
+                    listView1.asc = false;
+                else
+                    listView1.asc = true;
+                if(e.Column == 1 || e.Column == 4)
+                    listView1.ListViewItemSorter = new ListViewItemComparer(e.Column, listView1.asc, 0, 10);
+                else
+                    listView1.ListViewItemSorter = new ListViewItemComparer(e.Column, listView1.asc, 1, 0);
+            }
+            else
+            {
+                listView1.asc = false;
+                if (e.Column == 1 || e.Column == 4)
+                    listView1.ListViewItemSorter = new ListViewItemComparer(e.Column, listView1.asc, 0, 10);
+                else
+                    listView1.ListViewItemSorter = new ListViewItemComparer(e.Column, listView1.asc, 1, 0);
+                listView1.CurrentColumn = e.Column;
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            foreach(ListViewItem item in listView1.SelectedItems)
+            {
+                string url = "";
+                if((bool)item.Tag)
+                    url = "https://" + item.Text;
+                else
+                    url = "http://" + item.Text;
+                Process.Start(url);
+            }
+        }
+    }
+}
